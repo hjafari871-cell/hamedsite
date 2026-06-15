6dfc3426526f68a5e053bed841d83c2227eec6dc
Hamed <hjafari871@gmail.com>
2026-06-15 23:25:20 +0330

    my personal site

---
+++ website/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from website.views import *
+app_name = "website"
+
+urlpatterns = [
+    path("", index_view, name="index"),
+    path("about", about_view, name="about"),
+    path("contact", contact_view, name="contact"),
+    path("portfolio-details", portfolio_details_view, name="portfolio-details"),
+    path("portfolio", portfolio_view, name="portfolio"),
+    path("resume", resume_view, name="resume"),
+    path("service-details", service_details_view, name="Service-details"),
+    path("service", service_view, name="service"),
+    path("starter-page",starter_page, name="starter-page"),
+]
---
+++ website/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request,'website/index.html')
+
+
+def about_view(request):
+    context = {'Birthday':'5 sep 1995'}
+    return render(request, "website/about.html", context)
+
+
+def contact_view(request):
+    return render(request, "website/contact.html")
+
+
+def portfolio_view(request):
+    return render(request, "website/portfolio.html")
+
+
+def portfolio_details_view(request):
+    return render(request, "website/portfolio-details.html")
+
+
+def resume_view(request):
+    return render(request, "website/resume.html")
+
+
+def service_view(request):
+    return render(request, "website/services.html")
+
+
+def service_details_view(request):
+    return render(request, "website/service-details.html")
+
+
+def starter_page(request):
+    return render(request, "website/starter-page.html")
